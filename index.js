3c555242c18ad5dfe2e8b73342aa5cda8ae23e20
Alston Lin <alstonlin812@gmail.com>
2018-10-20 04:12:58 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express'),
+  app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World',
+  });
+});
+app.listen(3000, () => console.log('Listening on port 3000'));
